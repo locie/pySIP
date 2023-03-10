043a5c3cdde7709389d8257431f30590bcd9bf67
Nicolas Cellier <contact@celliern.io>
2023-03-10 11:21:03 +0100

    add namespace-like interface for models

--- pysip/statespace/__init__.py
+++ pysip/statespace/__init__.py
@@ -5,3 +5,22 @@ from .latent_force_model import LatentForceModel, R2C2_Qgh_Matern32
 from .meta import MetaStateSpace, model_registry, statespace
 from .nodes import Io, Node, Par
 from .thermal_network import *
+
+Models = model_registry
+
+__all__ = [
+    "GPModel",
+    "RCModel",
+    "StateSpace",
+    "GPProduct",
+    "GPSum",
+    "LatentForceModel",
+    "R2C2_Qgh_Matern32",
+    "MetaStateSpace",
+    "model_registry",
+    "statespace",
+    "Io",
+    "Node",
+    "Par",
+    "Models",
+]
