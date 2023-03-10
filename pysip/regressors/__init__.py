99783685e608b1b73ab62ca7bafbed6be2a6cefa
Nicolas Cellier <contact@celliern.io>
2023-03-10 11:24:28 +0100

    add alls for init files

--- pysip/regressors/__init__.py
+++ pysip/regressors/__init__.py
@@ -1,3 +1,9 @@
 from .base import BaseRegressor
 from .bayesian import BayesRegressor
 from .frequentist import FreqRegressor
+
+__all__ = [
+    "BaseRegressor",
+    "BayesRegressor",
+    "FreqRegressor",
+]
--- pysip/state_estimator/__init__.py
+++ pysip/state_estimator/__init__.py
@@ -1,2 +1,7 @@
 from .base import BayesianFilter
 from .kalman_qr import Kalman_QR
+
+__all__ = [
+    "BayesianFilter",
+    "Kalman_QR",
+]
