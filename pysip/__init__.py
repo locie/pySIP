d72d85f693331a870700c3199e8d7c9ab70c7b44
Nicolas Cellier <contact@celliern.io>
2024-05-06 16:13:47 +0200

    use pdm and release upper bounds

--- pysip/__init__.py
+++ pysip/__init__.py
@@ -1,3 +1,9 @@
+import numpy as np
+
+try:
+    np.distutils.__config__.blas_opt_info = np.distutils.__config__.blas_ilp64_opt_info
+except Exception:
+    pass
 import importlib.metadata
 
 __version__ = importlib.metadata.version(__package__ or __name__)
